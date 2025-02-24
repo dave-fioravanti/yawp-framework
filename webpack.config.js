22f8cd96edf7a2d721c8fdbbb6750098b871e1cd
Dave Fioravanti <davidafioravanti@gmail.com>
2025-02-24 10:12:39 -0500

    Update package dependencies, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,12 @@ module.exports = {
                 use: [
                     { loader: "style-loader" },
                     { loader: "css-loader" },
-                    { loader: "sass-loader" }
+                    { 
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
                 ]
             },
             { 
